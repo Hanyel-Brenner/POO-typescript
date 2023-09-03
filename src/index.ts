106d00a439e495b82abecf06eb54c66fab29635b
Hanyel-Brenner <125314727+Hanyel-Brenner@users.noreply.github.com>
2023-09-03 19:59:28 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,22 @@
-import {Bike} from "./bike"
-import { Cliente } from "./cliente";
-import { Reserva } from "./reserva";
+import { App } from "./app";
+import { Bike } from "./bike";
+import { Rent } from "./rent";
+import { User } from "./user";
 
+const bike = new Bike('mountain bike', 'mountain', 
+    123, 500, 100.5, 'desc', 5, [])
+const user = new User('Maria', 'maria@mail.com', '1234')
+const today = new Date()
+const twoDaysFromToday = new Date()
+twoDaysFromToday.setDate(twoDaysFromToday.getDate() + 2)
+const tomorrow = new Date()
+tomorrow.setDate(tomorrow.getDate() + 1)
+const sevenDaysFromToday = new Date()
+sevenDaysFromToday.setDate(sevenDaysFromToday.getDate() + 7)
+const rent1 = Rent.create([], bike, user, today, twoDaysFromToday)
+const user2 = new User('Maria Clara', 'maria@mail.com', '3123')
 
-let bike:Bike = new Bike(10);
-let cliente:Cliente = new Cliente("Hanyel");
-let reserva = new Reserva();
-reserva.Reservar(cliente,bike,new Date(2023,12,12), new Date(2023,12,14));
-let dias:number = reserva.CalcularCusto();
-console.log(typeof(dias));
-console.log(reserva.bike.valorAluguel);
+const app = new App()
+app.registerUser(user)
+
+console.log(app.findUser('maria@mail.com'))
