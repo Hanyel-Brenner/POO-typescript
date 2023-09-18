fd1b5421e669e62a5bcc996ed9f07191fcd7657e
Hanyel-Brenner <125314727+Hanyel-Brenner@users.noreply.github.com>
2023-09-17 21:47:01 -0300

    Update rent.ts

--- src/rent.ts
+++ src/rent.ts
@@ -1,32 +1,25 @@
 import { Bike } from "./bike";
 import { User } from "./user";
 
+
 export class Rent {
+    public end:Date = undefined
     private constructor(
         public bike: Bike,
         public user: User,
-        public dateFrom: Date,
-        public dateTo: Date,
-        public dateReturned?: Date
+        public start:Date,
     ) {}
 
-    static create(rents: Rent[], bike: Bike, user: User, 
-                  startDate: Date, endDate: Date): Rent {
-        const canCreate = Rent.canRent(rents, startDate, endDate)
-        if (canCreate) return new Rent(bike, user, startDate, endDate)
-        //throw new Error('Overlapping dates.')
+    static create(rents: Rent[], bike: Bike, user: User, startDate: Date): Rent {
+
+        const canCreate = Rent.canRent(bike)
+        if (canCreate) return new Rent(bike, user, startDate)
+        else return null
     }
 
-    static canRent(rents: Rent[], startDate: Date, endDate: Date): boolean {
-
-        if(rents ==  undefined) { return true}
-
-        let j:number = 0;
-        for(let i:Rent = rents[j]; i!=undefined && i!= null; i = rents[j++]){
-            if(startDate <= i.dateTo && endDate >= i.dateFrom){
-                return false
-            }   
-                return true
+    static canRent(bike:Bike): boolean {
+        if(bike == null) return false
+        if(bike.available) return true
+        else return false
     }
-}
 }
