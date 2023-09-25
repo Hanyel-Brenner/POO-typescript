1f827d41dcd717c036e9995a81bc89cbd8db3e1d
Hanyel-Brenner <125314727+Hanyel-Brenner@users.noreply.github.com>
2023-09-25 00:48:30 -0300

    Update app.spec.ts

--- src/app.spec.ts
+++ src/app.spec.ts
@@ -3,8 +3,30 @@ import { App } from "./app"
 import { Bike } from "./bike"
 import { User } from "./user"
 import { Location } from "./location"
+import { UserNotFoundError } from "./errors/user-not-found-error"
+import { BikeNotFoundError } from "./errors/bike-not-found-error"
 
 describe('App', () => {
+    it('should throw exception when trying to find  an unregistered user',() => {
+        const app = new App()
+        expect(() => {app.findUser('innit@mail.com')}).toThrow(UserNotFoundError)
+    })
+    it('should throw exception when trying to find  an unregistered bike',() => {
+        const app = new App()
+        expect(() => {app.findBike('123')}).toThrow(BikeNotFoundError)
+    })
+
+    it('should throw an exception whent trying to rent an unaveilable bike', async()=> {
+        const app = new App()
+        const user = new User('Jose', 'jose@mail.com', '1234')
+        await app.registerUser(user)
+        const bike = new Bike('caloi mountainbike', 'mountain bike',
+            1234, 1234, 100.0, 'My bike', 5, [])
+        app.registerBike(bike)
+        app.rentBike(bike.id,user.email,new Date())
+
+    })
+
     it('should correctly calculate the rent amount', async () => {
         const app = new App()
         const user = new User('Jose', 'jose@mail.com', '1234')
@@ -31,14 +53,14 @@ describe('App', () => {
         expect(bike.location.longitude).toEqual(newYork.longitude)
     })
 
-    it('should throw an exception when trying to move an unregistered bike', () => {
+   /* it('should throw an exception when trying to move an unregistered bike', () => {
         const app = new App()
         const bike = new Bike('caloi mountainbike', 'mountain bike',
         1234, 1234, 100.0, 'My bike', 5, [])
         const newYork = new Location(40.753056, -73.983056)
         let t = app.moveBikeTo(bike.id,newYork)
-        expect( () => app.moveBikeTo(bike.id,newYork)).toThrow(Error)
-    })
+        expect( () => app.moveBikeTo(bike.id,newYork)).toThrow(BikeNotFoundError)
+    })*/
 
 
 })
